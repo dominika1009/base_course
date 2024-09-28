a87fa0f242db067c675881734a6ef99a4ec7972a
dominika1009 <eriklenlovi@gmail.com>
2024-09-28 10:00:35 +0000

    лаба

---
+++ task6.py
@@ -0,0 +1,15 @@
+x = []
+a = input('Введите ваш возраст:')
+b = input('Введите ваш пол:')
+c = input('Введите ваше имя:')
+d = input('Введите ваш город:')
+h = input('Введите ваш класс:')
+
+x.append (a)
+x.append (b)
+x.append (c)
+x.append (d)
+x.append (h)
+
+print('Ваша информация')
+print (x)
